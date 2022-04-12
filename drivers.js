09da67e497a10e898771309f4eab767e4e1078d0
scottbooma <scottbooma@gmail.com>
2022-04-12 15:55:18 -0400

    Add race and sprint results for drivers

--- drivers.js
+++ drivers.js
@@ -17,12 +17,17 @@ form.addEventListener("change", event => {
         return fetch(url)
             .then(response => response.json())
     })).then(driverStatsResponseArray => {
-        const driverInfo = driverStatsResponseArray[0].MRData.RaceTable.Races[0].Results[0].Driver
+        const driverInfo = driverStatsResponseArray[0].MRData.RaceTable.Races[0].Results[0]
         const raceResults = driverStatsResponseArray[0].MRData.RaceTable.Races
-        const qualiResults = driverStatsResponseArray[1].MRData.RaceTable.Races
         const sprintResults = driverStatsResponseArray[2].MRData.RaceTable.Races
-        console.log(driverInfo, raceResults, qualiResults, sprintResults)
-        addToDriverInfoContainer(getDriverImage(driverInfo))
+        getDriverImage(driverInfo)
+        getDriverInfo(driverInfo)
+        raceResults.forEach(raceResult => {
+            addRaceResults(getRaceResults(raceResult))
+        })
+        sprintResults.forEach(sprintResult => {
+            addSprintResults(getSprintResults(sprintResult))
+        })
     })
 })
 
@@ -38,16 +43,81 @@ fetch(standingsURL)
         )
     })
 
-function addToDriverInfoContainer(element) {
-    driverInfoContainer.append(element)
+function getDriverImage(driverInfo) {
+    const img = document.querySelector(".driver-image")
+    img.src = `images/drivers/${driverInfo.Driver.driverId}.jpg`
+    img.alt = `${driverInfo.Driver.givenName} ${driverInfo.Driver.familyName}`
 }
 
-function getDriverImage(driverInfo) {
-    const img = document.createElement("img")
-    img.classList.add("driver-image")
-    img.src = `images/drivers/${driverInfo.driverId}.jpg`
-    img.alt = `${driverInfo.givenName} ${driverInfo.familyName}`
-    return img
+function getDriverInfo(driverInfo) {
+    const div = document.querySelector(".driver-info")
+    div.innerHTML = `
+        <h2>${driverInfo.Driver.givenName} ${driverInfo.Driver.familyName}</h2>
+        <table>
+            <tr>
+                <td>Team</td>
+                <td>${driverInfo.Constructor.name}</td>
+            </tr>
+            <tr>
+                <td>Number</td>
+                <td>${driverInfo.Driver.permanentNumber}</td>
+            </tr>
+            <tr>
+                <td>Nationality</td>
+                <td>${driverInfo.Driver.nationality}</td>
+            </tr>
+            <tr>
+                <td>Date of Birth</td>
+                <td>${driverInfo.Driver.dateOfBirth}</td>
+            </tr>
+        </table>
+    `
+}
+
+function getRaceResults(raceResult) {
+    document.querySelector(".race-table").classList.remove("hidden")
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+        <td>${raceResult.raceName}</td>
+        <td>${accountForRaceDNF(raceResult)}</td>
+        <td>${raceResult.Results[0].points}</td>
+    `
+    return tr
+}
+
+function accountForRaceDNF(raceResult) {
+    if (raceResult.Results[0].status === "Finished") {
+        return raceResult.Results[0].position
+    } else {
+        return "DNF"
+    }
+}
+
+function getSprintResults(sprintResult) {
+    document.querySelector(".sprint-table").classList.remove("hidden")
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+        <td>${sprintResult.raceName}</td>
+        <td>${accountForSprintDNF(sprintResult)}</td>
+        <td>${sprintResult.SprintResults[0].points}</td>
+    `
+    return tr
+}
+
+function accountForSprintDNF(sprintResult) {
+    if (sprintResult.SprintResults[0].status === "Finished") {
+        return sprintResult.SprintResults[0].position
+    } else {
+        return "DNF"
+    }
+}
+
+function addRaceResults(element) {
+    document.querySelector(".race-table-body").append(element)
+}
+
+function addSprintResults(element) {
+    document.querySelector(".sprint-table-body").append(element)
 }
 
 function createDriverSelection(driverStanding) {
