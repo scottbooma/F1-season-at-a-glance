3193a548c33e9638b745a247a797ab378a67fa10
scottbooma <scottbooma@gmail.com>
2022-04-19 13:59:40 -0400

    Lint to the gods

--- index.js
+++ index.js
@@ -3,109 +3,122 @@ const scheduleURL = "https://ergast.com/api/f1/current.json"
 const calendar = document.querySelector(".calendar")
 const nextRaceContainer = document.querySelector(".nextRaceContainer")
 
-const currentDate = new Date().toISOString().slice(0, 10)
+const currentDate = new Date().toISOString()
+  .slice(0, 10)
 
-document.addEventListener("DOMContentLoaded", event => {
-    document.querySelector(".standings-link").classList.remove("selected-page")
-    document.querySelector(".index-link").classList.add("selected-page")
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".standings-link").classList.remove("selected-page")
+  document.querySelector(".index-link").classList.add("selected-page")
 })
 
 fetch(scheduleURL)
-    .then(response => response.json())
-    .then(raceListObject => {
-        const raceList = raceListObject.MRData.RaceTable.Races
-        raceList.forEach(race => {
-            addRaceToCalendar(createRaceForCalendar(race))
-        })
-        addNextRace(createNextRace(findNextRace(raceList)))
+  .then(response => response.json())
+  .then(raceListObject => {
+    const raceList = raceListObject.MRData.RaceTable.Races
+    raceList.forEach(race => {
+      addRaceToCalendar(createRaceForCalendar(race))
     })
-    .catch(error => {
-        console.error(error.message)
-        window.location.href = "404.html"
-    })
+    addNextRace(createNextRace(findNextRace(raceList)))
+  })
+  .catch(error => {
+    console.error(error.message)
+    window.location.href = "404.html"
+  })
 
-function findNextRace(raceList) {
-    return raceList.find(event => event.date >= currentDate)
+function findNextRace (raceList) {
+  return raceList.find(event => event.date >= currentDate)
 }
 
-function createNextRace(race) {
-    const nextRace = document.createElement("div")
-    nextRace.classList.add("nextRace")
-    if (Object.keys(race).includes("Sprint")) {
-        nextRace.innerHTML = `
+function createNextRace (race) {
+  const nextRace = document.createElement("div")
+  nextRace.classList.add("nextRace")
+  if (Object.keys(race).includes("Sprint")) {
+    nextRace.innerHTML = `
             <h2>${race.raceName}</h2>
             <p>Round ${race.round}</p>
             <p>${race.Circuit.circuitName}</p>
             <date>${convertDateAndTimeToLocal(race.date, race.time)}</date>
             <div class="weekend-schedule">
                 <div>FP1
-                    <date>${convertDateAndTimeToLocal(race.FirstPractice.date, race.FirstPractice.time)}</date>
+                    <date>${convertDateAndTimeToLocal(race.FirstPractice.date,
+    race.FirstPractice.time)}</date>
                 </div>
                 <div>Qualifying
-                    <date>${convertDateAndTimeToLocal(race.Qualifying.date, race.Qualifying.time)}</date>
+                    <date>${convertDateAndTimeToLocal(race.Qualifying.date,
+    race.Qualifying.time)}</date>
                 </div>
                 <div>FP2
-                    <date>${convertDateAndTimeToLocal(race.SecondPractice.date, race.SecondPractice.time)}</date>
+                    <date>${convertDateAndTimeToLocal(race.SecondPractice.date,
+    race.SecondPractice.time)}</date>
                 </div>
                 <div>Sprint
-                    <date>${convertDateAndTimeToLocal(race.Sprint.date, race.Sprint.time)}</date>
+                    <date>${convertDateAndTimeToLocal(race.Sprint.date,
+    race.Sprint.time)}</date>
                 </div>
             </div>
         `
-    } else {
-        nextRace.innerHTML = `
+  } else {
+    nextRace.innerHTML = `
         <h2>${race.raceName}</h2>
         <p>Round ${race.round}</p>
         <p>${race.Circuit.circuitName}</p>
         <date>${convertDateAndTimeToLocal(race.date, race.time)}</date>
         <div class="weekend-schedule">
             <div>FP1
-                <date>${convertDateAndTimeToLocal(race.FirstPractice.date, race.FirstPractice.time)}</date>
+                <date>${convertDateAndTimeToLocal(race.FirstPractice.date,
+    race.FirstPractice.time)}</date>
             </div>
             <div>FP2
-                <date>${convertDateAndTimeToLocal(race.SecondPractice.date, race.SecondPractice.time)}</date>
+                <date>${convertDateAndTimeToLocal(race.SecondPractice.date,
+    race.SecondPractice.time)}</date>
             </div>
             <div>FP3
-                <date>${convertDateAndTimeToLocal(race.ThirdPractice.date, race.ThirdPractice.time)}</date>
+                <date>${convertDateAndTimeToLocal(race.ThirdPractice.date,
+    race.ThirdPractice.time)}</date>
             </div>
             <div>Qualifying
-                <date>${convertDateAndTimeToLocal(race.Qualifying.date, race.Qualifying.time)}</date>
+                <date>${convertDateAndTimeToLocal(race.Qualifying.date,
+    race.Qualifying.time)}</date>
             </div>
         </div>
     `
-    }
-    return nextRace
+  }
+  return nextRace
 }
 
-function addNextRace(nextRace) {
-    nextRaceContainer.append(nextRace)
+function addNextRace (nextRace) {
+  nextRaceContainer.append(nextRace)
 }
 
-function createRaceForCalendar(event) {
-    const race = document.createElement("div")
-    race.classList.add("race")
-    if (event.date < currentDate) {
-        race.classList.add("pastRace")
-    }
-    race.innerHTML = `
+function createRaceForCalendar (event) {
+  const race = document.createElement("div")
+  race.classList.add("race")
+  if (event.date < currentDate) {
+    race.classList.add("pastRace")
+  }
+  race.innerHTML = `
         <h3>${event.raceName}</h3>
         <date>${convertDate(event.date)}</date>
         <p>${event.Circuit.circuitName}</p>
     `
-    return race
+  return race
 }
 
-function addRaceToCalendar(raceEvent) {
-    calendar.append(raceEvent)
+function addRaceToCalendar (raceEvent) {
+  calendar.append(raceEvent)
 }
 
 
-function convertDateAndTimeToLocal(date, time) {
-    formattedDate = new Date(`${date} ${time}`)
-    return `${formattedDate.toString().slice(0, 10)} ${formattedDate.toString().slice(16, 21)}`
+function convertDateAndTimeToLocal (date, time) {
+  const formattedDate = new Date(`${date} ${time}`)
+  return `${formattedDate
+    .toString()
+    .slice(0, 10)} ${formattedDate
+    .toString()
+    .slice(16, 21)}`
 }
 
-function convertDate(date) {
-    formattedDate = new Date(`${date}T00:00:00`)
-    return formattedDate.toString().slice(3, 15)
+function convertDate (date) {
+  const formattedDate = new Date(`${date}T00:00:00`)
+  return formattedDate.toString().slice(3, 15)
 }
--- standings.js
+++ standings.js
@@ -1,81 +1,82 @@
 const driverStandingsURL = "https://ergast.com/api/f1/current/driverStandings.json"
 const constructorStandingsURL = "https://ergast.com/api/f1/current/constructorstandings.json"
-const driverStatsBaseURL = "https://ergast.com/api/f1/current/drivers/"  // need to append ${driver}/(results for race)(qualifying for quali)(sprint for sprint).json
+const driverStatsBaseURL = "https://ergast.com/api/f1/current/drivers/"
 
 const driverSelector = document.querySelector("#drivers")
 const form = document.querySelector("form")
-const driverInfoContainer = document.querySelector(".driver-info-container")
 
-document.addEventListener("DOMContentLoaded", event => {
-    document.querySelector(".index-link").classList.remove("selected-page")
-    document.querySelector(".standings-link").classList.add("selected-page")
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".index-link").classList.remove("selected-page")
+  document.querySelector(".standings-link").classList.add("selected-page")
 })
 
 
 form.addEventListener("change", event => {
-    event.preventDefault()
-    const driverStatsRequestArray = [
-        `${driverStatsBaseURL}${event.target.value}/results.json`,
-        `${driverStatsBaseURL}${event.target.value}/sprint.json`
-    ]
-    Promise.all(driverStatsRequestArray.map(url => {
-        return fetch(url)
-            .then(response => response.json())
-    })).then(driverStatsResponseArray => {
-        const driverInfo = driverStatsResponseArray[0].MRData.RaceTable.Races[0].Results[0]
-        const raceResults = driverStatsResponseArray[0].MRData.RaceTable.Races
-        const sprintResults = driverStatsResponseArray[1].MRData.RaceTable.Races
-        getDriverImage(driverInfo)
-        getDriverInfo(driverInfo)
-        clearResultsTables()
-        raceResults.forEach(raceResult => {
-            addRaceResults(getRaceResults(raceResult))
-        })
-        sprintResults.forEach(sprintResult => {
-            addSprintResults(getSprintResults(sprintResult))
-        })
+  event.preventDefault()
+  const driverStatsRequestArray = [
+    `${driverStatsBaseURL}${event.target.value}/results.json`,
+    `${driverStatsBaseURL}${event.target.value}/sprint.json`,
+  ]
+  Promise.all(driverStatsRequestArray.map(url => {
+    return fetch(url)
+      .then(response => response.json())
+  })).then(driverStatsArray => {
+    const driverInfo = driverStatsArray[0].MRData.RaceTable.Races[0].Results[0]
+    const raceResults = driverStatsArray[0].MRData.RaceTable.Races
+    const sprintResults = driverStatsArray[1].MRData.RaceTable.Races
+    getDriverImage(driverInfo)
+    getDriverInfo(driverInfo)
+    clearResultsTables()
+    raceResults.forEach(raceResult => {
+      addRaceResults(getRaceResults(raceResult))
     })
-        .catch(error => {
-            console.error(error.message)
-            window.location.href = "404.html"
-        })
+    sprintResults.forEach(sprintResult => {
+      addSprintResults(getSprintResults(sprintResult))
+    })
+  })
+    .catch(error => {
+      console.error(error.message)
+      window.location.href = "404.html"
+    })
 })
 
 
 fetch(driverStandingsURL)
-    .then(response => response.json())
-    .then(driverStandingsObject => {
-        const driverStandingsList = driverStandingsObject.MRData.StandingsTable.StandingsLists[0].DriverStandings
-        driverStandingsList.forEach(driverStanding => {
-            addDriverSelection(createDriverSelection(driverStanding))
-            addDriverStandingListing(createDriverStandingListing(driverStanding))
-        }
-        )
-    })
-    .catch(error => {
-        console.error(error.message)
-        window.location.href = "404.html"
-    })
+  .then(response => response.json())
+  .then(driverStandingsObject => {
+    const driverStandingsList = driverStandingsObject.MRData.StandingsTable.StandingsLists[0].DriverStandings
+    driverStandingsList.forEach(driverStanding => {
+      addDriverSelection(createDriverSelection(driverStanding))
+      addDriverStandingListing(createDriverStandingListing(driverStanding))
+    },
+    )
+  })
+  .catch(error => {
+    console.error(error.message)
+    window.location.href = "404.html"
+  })
 
 fetch(constructorStandingsURL)
-    .then(response => response.json())
-    .then(constructorStandingsObject => {
-        const constructorStandingsList = constructorStandingsObject.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
-        constructorStandingsList.forEach(constructorStanding => {
-            addConstructorStandingListing(createConstructorStandingListing(constructorStanding))
-        }
-        )
-    })
+  .then(response => response.json())
+  .then(constructorStandingsObject => {
+    const constructorStandingsList = constructorStandingsObject.MRData.StandingsTable.StandingsLists[0].ConstructorStandings
+    constructorStandingsList.forEach(constructorStanding => {
+      addConstructorStandingListing(
+        createConstructorStandingListing(constructorStanding),
+      )
+    },
+    )
+  })
 
-function getDriverImage(driverInfo) {
-    const img = document.querySelector(".driver-image")
-    img.src = `images/drivers/${driverInfo.Driver.driverId}.jpg`
-    img.alt = `${driverInfo.Driver.givenName} ${driverInfo.Driver.familyName}`
+function getDriverImage (driverInfo) {
+  const img = document.querySelector(".driver-image")
+  img.src = `images/drivers/${driverInfo.Driver.driverId}.jpg`
+  img.alt = `${driverInfo.Driver.givenName} ${driverInfo.Driver.familyName}`
 }
 
-function getDriverInfo(driverInfo) {
-    const div = document.querySelector(".driver-info")
-    div.innerHTML = `
+function getDriverInfo (driverInfo) {
+  const div = document.querySelector(".driver-info")
+  div.innerHTML = `
         <h2>${driverInfo.Driver.givenName} ${driverInfo.Driver.familyName}</h2>
         <table>
             <tr>
@@ -98,99 +99,105 @@ function getDriverInfo(driverInfo) {
     `
 }
 
-function getRaceResults(raceResult) {
-    document.querySelector(".race-table").classList.remove("hidden")
-    const tr = document.createElement("tr")
-    tr.innerHTML = `
+function getRaceResults (raceResult) {
+  document.querySelector(".race-table").classList.remove("hidden")
+  const tr = document.createElement("tr")
+  tr.innerHTML = `
         <td>${raceResult.raceName}</td>
         <td>${accountForRaceDNF(raceResult)}</td>
         <td>${raceResult.Results[0].points}</td>
     `
-    return tr
+  return tr
 }
 
-function accountForRaceDNF(raceResult) {
-    if (raceResult.Results[0].status === "Finished" || raceResult.Results[0].status.includes("Lap")) {
-        return raceResult.Results[0].position
-    } else {
-        return "DNF"
-    }
+function accountForRaceDNF (raceResult) {
+  if (raceResult.Results[0].status === "Finished" ||
+    raceResult.Results[0].status.includes("Lap")) {
+    return raceResult.Results[0].position
+  } else {
+    return "DNF"
+  }
 }
 
-function getSprintResults(sprintResult) {
-    document.querySelector(".sprint-table").classList.remove("hidden")
-    const tr = document.createElement("tr")
-    tr.innerHTML = `
+function getSprintResults (sprintResult) {
+  document.querySelector(".sprint-table").classList.remove("hidden")
+  const tr = document.createElement("tr")
+  tr.innerHTML = `
         <td>${sprintResult.raceName}</td>
         <td>${accountForSprintDNF(sprintResult)}</td>
         <td>${sprintResult.SprintResults[0].points}</td>
     `
-    return tr
+  return tr
 }
 
-function accountForSprintDNF(sprintResult) {
-    if (sprintResult.SprintResults[0].status === "Finished") {
-        return sprintResult.SprintResults[0].position
-    } else {
-        return "DNF"
-    }
+function accountForSprintDNF (sprintResult) {
+  if (sprintResult.SprintResults[0].status === "Finished") {
+    return sprintResult.SprintResults[0].position
+  } else {
+    return "DNF"
+  }
 }
 
-function addRaceResults(element) {
-    document.querySelector(".race-table-body").append(element)
+function addRaceResults (element) {
+  document.querySelector(".race-table-body").append(element)
 }
 
-function addSprintResults(element) {
-    document.querySelector(".sprint-table-body").append(element)
+function addSprintResults (element) {
+  document.querySelector(".sprint-table-body").append(element)
 }
 
-function clearResultsTables() {
-    document.querySelector(".race-table-body").innerHTML = ``
-    document.querySelector(".sprint-table-body").innerHTML = ``
+function clearResultsTables () {
+  document.querySelector(".race-table-body").innerHTML = ""
+  document.querySelector(".sprint-table-body").innerHTML = ""
 }
 
-function createDriverSelection(driverStanding) {
-    const driverSelection = document.createElement("option")
-    driverSelection.value = `${driverStanding.Driver.driverId} `
-    driverSelection.textContent = `${driverStanding.Driver.givenName} ${driverStanding.Driver.familyName} `
-    return driverSelection
+function createDriverSelection (driverStanding) {
+  const driverSelection = document.createElement("option")
+  driverSelection.value = `${driverStanding.Driver.driverId} `
+  driverSelection.textContent = `${driverStanding.Driver.givenName} ${driverStanding.Driver.familyName} `
+  return driverSelection
 }
 
-function addDriverSelection(driver) {
-    driverSelector.append(driver)
+function addDriverSelection (driver) {
+  driverSelector.append(driver)
 }
 
-function createDriverStandingListing(driverStanding) {
-    const driver = document.createElement("tr")
-    driver.innerHTML = `
+function createDriverStandingListing (driverStanding) {
+  const driver = document.createElement("tr")
+  driver.innerHTML = `
         <td> ${driverStanding.position}</td>
-        <td><img src="images/constructors/${driverStanding.Constructors[0].constructorId}.png" alt="${driverStanding.Constructors[0].constructorId}" class="standings-logo"/></td>
+        <td><img src="images/constructors/${driverStanding.Constructors[0].constructorId}.png" 
+          alt="${driverStanding.Constructors[0].constructorId}" 
+          class="standings-logo"/></td>
         <td>${driverStanding.Driver.code}</td>
         <td>${driverStanding.points}</td>
     `
-    return driver
+  return driver
 }
 
-function addDriverStandingListing(driver) {
-    document.querySelector(".driver-standings-table-body").append(driver)
+function addDriverStandingListing (driver) {
+  document.querySelector(".driver-standings-table-body").append(driver)
 }
 
-function createConstructorStandingListing(constructorStanding) {
-    const constructor = document.createElement("tr")
-    constructor.innerHTML = `
+function createConstructorStandingListing (constructorStanding) {
+  const constructor = document.createElement("tr")
+  constructor.innerHTML = `
         <td> ${constructorStanding.position}</td>
-        <td><img src="images/constructors/${constructorStanding.Constructor.constructorId}.png" alt="${constructorStanding.Constructor.constructorId}" class="standings-logo"/></td>
+        <td><img src="images/constructors/${constructorStanding.Constructor.constructorId}.png" 
+          alt="${constructorStanding.Constructor.constructorId}" 
+          class="standings-logo"/></td>
         <td>${constructorStanding.Constructor.name}</td>
         <td>${constructorStanding.points}</td>
     `
-    return constructor
+  return constructor
 }
 
-function addConstructorStandingListing(constructor) {
-    document.querySelector(".constructor-standings-table-body").append(constructor)
+function addConstructorStandingListing (constructor) {
+  document.querySelector(".constructor-standings-table-body")
+    .append(constructor)
 }
 
-function convertDate(date) {
-    formattedDate = new Date(`${date}T00:00:00`)
-    return formattedDate.toString().slice(3, 15)
+function convertDate (date) {
+  const formattedDate = new Date(`${date}T00:00:00`)
+  return formattedDate.toString().slice(3, 15)
 }
