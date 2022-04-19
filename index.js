bcd0e764f59694aae640b1bec07a314d2df7403c
scottbooma <scottbooma@gmail.com>
2022-04-19 14:03:33 -0400

    Add responsive styling to 404 page

--- index.js
+++ index.js
@@ -4,36 +4,36 @@ const calendar = document.querySelector(".calendar")
 const nextRaceContainer = document.querySelector(".nextRaceContainer")
 
 const currentDate = new Date().toISOString()
-  .slice(0, 10)
+    .slice(0, 10)
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".standings-link").classList.remove("selected-page")
-  document.querySelector(".index-link").classList.add("selected-page")
+    document.querySelector(".standings-link").classList.remove("selected-page")
+    document.querySelector(".index-link").classList.add("selected-page")
 })
 
 fetch(scheduleURL)
-  .then(response => response.json())
-  .then(raceListObject => {
-    const raceList = raceListObject.MRData.RaceTable.Races
-    raceList.forEach(race => {
-      addRaceToCalendar(createRaceForCalendar(race))
+    .then(response => response.json())
+    .then(raceListObject => {
+        const raceList = raceListObject.MRData.RaceTable.Races
+        raceList.forEach(race => {
+            addRaceToCalendar(createRaceForCalendar(race))
+        })
+        addNextRace(createNextRace(findNextRace(raceist)))
     })
-    addNextRace(createNextRace(findNextRace(raceList)))
-  })
-  .catch(error => {
-    console.error(error.message)
-    window.location.href = "404.html"
-  })
+    .catch(error => {
+        console.error(error.message)
+        window.location.href = "404.html"
+    })
 
-function findNextRace (raceList) {
-  return raceList.find(event => event.date >= currentDate)
+function findNextRace(raceList) {
+    return raceList.find(event => event.date >= currentDate)
 }
 
-function createNextRace (race) {
-  const nextRace = document.createElement("div")
-  nextRace.classList.add("nextRace")
-  if (Object.keys(race).includes("Sprint")) {
-    nextRace.innerHTML = `
+function createNextRace(race) {
+    const nextRace = document.createElement("div")
+    nextRace.classList.add("nextRace")
+    if (Object.keys(race).includes("Sprint")) {
+        nextRace.innerHTML = `
             <h2>${race.raceName}</h2>
             <p>Round ${race.round}</p>
             <p>${race.Circuit.circuitName}</p>
@@ -41,24 +41,24 @@ function createNextRace (race) {
             <div class="weekend-schedule">
                 <div>FP1
                     <date>${convertDateAndTimeToLocal(race.FirstPractice.date,
-    race.FirstPractice.time)}</date>
+            race.FirstPractice.time)}</date>
                 </div>
                 <div>Qualifying
                     <date>${convertDateAndTimeToLocal(race.Qualifying.date,
-    race.Qualifying.time)}</date>
+                race.Qualifying.time)}</date>
                 </div>
                 <div>FP2
                     <date>${convertDateAndTimeToLocal(race.SecondPractice.date,
-    race.SecondPractice.time)}</date>
+                    race.SecondPractice.time)}</date>
                 </div>
                 <div>Sprint
                     <date>${convertDateAndTimeToLocal(race.Sprint.date,
-    race.Sprint.time)}</date>
+                        race.Sprint.time)}</date>
                 </div>
             </div>
         `
-  } else {
-    nextRace.innerHTML = `
+    } else {
+        nextRace.innerHTML = `
         <h2>${race.raceName}</h2>
         <p>Round ${race.round}</p>
         <p>${race.Circuit.circuitName}</p>
@@ -66,59 +66,59 @@ function createNextRace (race) {
         <div class="weekend-schedule">
             <div>FP1
                 <date>${convertDateAndTimeToLocal(race.FirstPractice.date,
-    race.FirstPractice.time)}</date>
+            race.FirstPractice.time)}</date>
             </div>
             <div>FP2
                 <date>${convertDateAndTimeToLocal(race.SecondPractice.date,
-    race.SecondPractice.time)}</date>
+                race.SecondPractice.time)}</date>
             </div>
             <div>FP3
                 <date>${convertDateAndTimeToLocal(race.ThirdPractice.date,
-    race.ThirdPractice.time)}</date>
+                    race.ThirdPractice.time)}</date>
             </div>
             <div>Qualifying
                 <date>${convertDateAndTimeToLocal(race.Qualifying.date,
-    race.Qualifying.time)}</date>
+                        race.Qualifying.time)}</date>
             </div>
         </div>
     `
-  }
-  return nextRace
+    }
+    return nextRace
 }
 
-function addNextRace (nextRace) {
-  nextRaceContainer.append(nextRace)
+function addNextRace(nextRace) {
+    nextRaceContainer.append(nextRace)
 }
 
-function createRaceForCalendar (event) {
-  const race = document.createElement("div")
-  race.classList.add("race")
-  if (event.date < currentDate) {
-    race.classList.add("pastRace")
-  }
-  race.innerHTML = `
+function createRaceForCalendar(event) {
+    const race = document.createElement("div")
+    race.classList.add("race")
+    if (event.date < currentDate) {
+        race.classList.add("pastRace")
+    }
+    race.innerHTML = `
         <h3>${event.raceName}</h3>
         <date>${convertDate(event.date)}</date>
         <p>${event.Circuit.circuitName}</p>
     `
-  return race
+    return race
 }
 
-function addRaceToCalendar (raceEvent) {
-  calendar.append(raceEvent)
+function addRaceToCalendar(raceEvent) {
+    calendar.append(raceEvent)
 }
 
 
-function convertDateAndTimeToLocal (date, time) {
-  const formattedDate = new Date(`${date} ${time}`)
-  return `${formattedDate
-    .toString()
-    .slice(0, 10)} ${formattedDate
-    .toString()
-    .slice(16, 21)}`
+function convertDateAndTimeToLocal(date, time) {
+    const formattedDate = new Date(`${date} ${time}`)
+    return `${formattedDate
+        .toString()
+        .slice(0, 10)} ${formattedDate
+            .toString()
+            .slice(16, 21)}`
 }
 
-function convertDate (date) {
-  const formattedDate = new Date(`${date}T00:00:00`)
-  return formattedDate.toString().slice(3, 15)
+function convertDate(date) {
+    const formattedDate = new Date(`${date}T00:00:00`)
+    return formattedDate.toString().slice(3, 15)
 }
